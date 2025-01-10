c8df5a6a40991b49e73651e54e80dd589151b6c1
Neto test <nikolajeliseev834@gmail.com>
2025-01-10 22:14:27 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant > 0) {
+    arr[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
+  } else if (discriminant === 0) {
+    arr[0] = -b / (2 * a);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100 / countMonths;
+  amount -= contribution;
+
+  let mounthPayment =
+    amount * (percent + percent / ((1 + percent) ** countMonths - 1));
+  let totalPaymentSum = mounthPayment * countMonths;
+  console.log(Number(totalPaymentSum.toFixed(2)));
 }
+
+calculateTotalMortgage(10, 0, 50000, 12);
+calculateTotalMortgage(10, 1000, 50000, 12);
+calculateTotalMortgage(10, 0, 20000, 24);
+calculateTotalMortgage(10, 1000, 20000, 24);
+calculateTotalMortgage(10, 20000, 20000, 24);
+calculateTotalMortgage(10, 0, 10000, 36);
+calculateTotalMortgage(15, 0, 10000, 36);
