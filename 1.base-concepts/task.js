13e653e523d6e0e8d557cef37ee794ce4f96cd4b
Neto test <nikolajeliseev834@gmail.com>
2025-01-11 11:35:20 +0300

    Исправлены замечания по работе

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -10,18 +10,17 @@ function solveEquation(a, b, c) {
     arr[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
   } else if (discriminant === 0) {
     arr[0] = -b / (2 * a);
-  } else {
-    arr = [];
   }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent = (percent / 100) / 12;
-  amount -= contribution;
+  let percentRate = (percent / 100) / 12;
+  let creditBody = amount - contribution;
 
   let mounthPayment =
-    amount * (percent + percent / ((1 + percent) ** countMonths - 1));
+    creditBody * (percentRate + percentRate / ((1 + percentRate) ** countMonths - 1));
   let totalPaymentSum = mounthPayment * countMonths;
   return Number(totalPaymentSum.toFixed(2));
 }
