3ffea9e833de7a4e772038f0264ba33e696971f7
Neto test <nikolajeliseev834@gmail.com>
2025-01-14 21:19:56 +0300

    Корректура 5-ой функции, добавлено условие для пустого массива

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -38,6 +38,8 @@ function differenceEvenOddWorker(...arr) {
   let sumEvenElement = 0;
   let sumOddElement = 0;
 
+  if (arr.length === 0) return 0;
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       sumEvenElement += arr[i];
@@ -52,6 +54,8 @@ function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0;
   let countEvenElement = 0;
 
+  if (arr.length === 0) return 0;
+
     for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       sumEvenElement += arr[i];
@@ -60,6 +64,9 @@ function averageEvenElementsWorker(...arr) {
   }
   return sumEvenElement / countEvenElement;
 }
+
+console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); // [2, 4, 6, 8] => 5
+console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // [64, 10, 40] => 38
 
 function makeWork (arrOfArr, func) {
   let maxWorkerResult = func(...arrOfArr[0]);
@@ -74,8 +81,3 @@ function makeWork (arrOfArr, func) {
   return maxWorkerResult;
 }
 
-const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
-console.log(makeWork(arr, summElementsWorker)); // максимум из 61, 206, 328, 284 => 328
-console.log(makeWork(arr, differenceMaxMinWorker)); // максимум из 10, 86, 44, 66 => 86
-console.log(makeWork(arr, differenceEvenOddWorker)); // максимум из 39, -6, -184, 92 => 92
-console.log(makeWork(arr, averageEvenElementsWorker)); // максимум из 12.5, 33.333, 72, 62.666 => 72
