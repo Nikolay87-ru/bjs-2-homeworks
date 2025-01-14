28a243c3f274c8b247216e3682364b4488b9397e
Neto test <nikolajeliseev834@gmail.com>
2025-01-14 18:33:05 +0300

    Решена 2 функция

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,15 +1,24 @@
-function getArrayParams(...arr) {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const totalSum = arr.reduce((x, y) => x + y);
-  const avg = totalSum / arr.length;
+// function getArrayParams(...arr) {
+//   const min = Math.min(...arr);
+//   const max = Math.max(...arr);
+//   const totalSum = arr.reduce((x, y) => x + y);
+//   const avg = totalSum / arr.length;
 
-  return { min: min, max: max, avg: +avg.toFixed(2) };
+//   return { min: min, max: max, avg: +avg.toFixed(2) };
+// }
+
+function summElementsWorker(...arr) {
+  let arraySum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    arraySum += arr[i];
+  }
+
+  return arraySum;
 }
 
-// function summElementsWorker(...arr) {
-
-// }
+console.log(summElementsWorker()); 
+console.log(summElementsWorker(10, 10, 11, 20, 10));
 
 // function differenceMaxMinWorker(...arr) {
 
