fe0d254c45182d59fb90225d20daf8a449ae701f
Neto test <nikolajeliseev834@gmail.com>
2025-01-27 17:52:40 +0300

    Задача №2 проверка сторон треугольника

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -4,12 +4,10 @@
   if (Number.isNaN(number)) {
     throw new Error("Невалидное значение");
   }
-
   return number;
 };
 
 const validateCount = (value) => {
-  debugger;
   try {
     return parseCount(value);
   } catch (error) {
@@ -17,5 +15,25 @@ const validateCount = (value) => {
   }
 };
 
-console.log(validateCount("56.65"));
-console.log(validateCount("ыфва"));
+// ЗАДАЧА №2 - ТРЕУГОЛЬНИК
+
+class Triangle {
+  constructor(sideOne, sideTwo, sideThree) {
+    this.sideOne = sideOne;
+    this.sideTwo = sideTwo;
+    this.sideThree = sideThree;
+  }
+
+  toBeDefined() {
+    let result1 = this.sideOne + this.sideTwo < this.sideThree;
+    let result2 = this.sideOne + this.sideThree < this.sideTwo;
+    let result3 = this.sideTwo + this.sideThree < this.sideOne;
+
+    if (result1 || result2 || result3) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+}
+
+console.log(new Triangle(1,3,3).toBeDefined());
