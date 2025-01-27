67426a602c5ebbe1b7dfdb45af461c3f1129551f
Neto test <nikolajeliseev834@gmail.com>
2025-01-27 14:57:54 +0300

    initial commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,23 @@
-﻿
+﻿const parseCount = (stringNum) => {
+  const number = Number.parseFloat(stringNum);
+
+  if (Number.isNaN(number)) {
+    throw new Error('Ввод не является числом в десятичной системе счисления');
+  }
+
+  return number;
+} 
+
+console.log(parseCount("123"));
+console.log(parseCount("0.12"));
+
+try {
+  console.log(parseCount("test"));
+} catch (error) {
+  console.error(error.message);
+}
+
+
+function validateCount() {
+  
+} 
