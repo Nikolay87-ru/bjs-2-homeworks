43266eca4272ecb45291ce25d0239ed9bc948314
Neto test <nikolajeliseev834@gmail.com>
2025-02-05 11:53:55 +0300

    Проверка метода removeClock

--- 7.async/task.js
+++ 7.async/task.js
@@ -5,27 +5,40 @@ class AlarmClock {
   }
 
   addClock(time, callback) {
-
     if (!time || !callback) {
-      throw new Error('Отсутствуют обязательные аргументы');
+      throw new Error("Отсутствуют обязательные аргументы");
     }
-    
-    if (this.alarmCollection.some(setClock => setClock.time === time)) {
-      console.warn('Уже присутствует звонок на это же время');
+
+    if (this.alarmCollection.some((setClock) => setClock.time === time)) {
+      console.warn("Уже присутствует звонок на это же время");
       return;
     }
 
-    this.alarmCollection.push({callback, time, canCall: true});
+    this.alarmCollection.push({ callback, time, canCall: true });
+  }
+
+  removeClock(time) {
+    if (
+      this.alarmCollection.some((setClock) => {
+        if (setClock.time === time) {
+          setClock.time = null;
+        }
+      })
+    ) {
+      return;
+    }
   }
 }
 
 const clock = new AlarmClock();
 
-const callback = f => f;
+const callback = (f) => f;
 
 clock.addClock("16:45", callback);
 clock.addClock("16:45", callback);
 
 console.log(clock.alarmCollection);
 
+clock.removeClock("16:45");
 
+console.log(clock.alarmCollection);
