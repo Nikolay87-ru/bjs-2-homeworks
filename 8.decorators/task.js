9d0262ae65b858a5d516647c8435096dbdf59492
Neto test <nikolajeliseev834@gmail.com>
2025-02-08 12:33:39 +0300

    initial commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,69 @@
 //Задача № 1
+const md5 = require("./js-md5.js");
+
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  const maxCacheValuesCount = 5;
+
+  return function(...args) {
+    const hash = md5(args);
+
+    let hashInCash;
+    cache.find(item => {
+      if (item.hash === hash) {
+        hashInCash = item.value;
+      }
+    });
+
+    if (hashInCash) {
+      console.log(`Вычисляем: ${hashInCash}`);
+      return `Вычисляем: ${hashInCash}`;
+    }
+
+    let result = func(...args);
+    cache.push({ hash: hash, value: result });
+
+    if (cache.length > maxCacheValuesCount) {
+      cache.shift();
+    }
+
+    return result;
+  };
 }
+
+const hasingText = "какой-нибудь текст";
+console.log(md5(hasingText)); // 8d1d3ecc455a4220590e6d27e6c1a267
+console.log(md5([10, 20, 30]));
+
+module.exports = {
+  cachingDecoratorNew,
+};
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    wrapper.allCount += 1;
+
+    if (wrapper.count === 0) {
+      func(...args);
+      wrapper.count += 1;
+    }
+
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count += 1;
+    }, delay);
+  }
+
+  return wrapper;
 }
+
+module.exports = {
+  debounceDecoratorNew,
+};
